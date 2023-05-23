5aeb339bdc6ce9b5f68c98d323df496fa1008a46
alisharafi88 <alisharafi893@gmail.com>
2023-05-23 05:19:10 +0430

    add category

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import ProductModel
+from .models import ProductModel, CategoryModel
 
 
 @admin.register(ProductModel)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'active', 'datetime_modified']
+
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- products/models.py
+++ products/models.py
@@ -4,13 +4,27 @@ from django.utils.translation import gettext as _
 from ckeditor.fields import RichTextField
 
 
+class CategoryModel(models.Model):
+    title = models.CharField(_('Title'), max_length=50)
+    description = RichTextField(_('Description', ), blank=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
 class ProductModel(models.Model):
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, related_name='category', null=False)
     title = models.CharField(_('Title'), max_length=50)
     description = RichTextField(_('Description', ))
     short_description = models.TextField(blank=True)
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     cover = models.ImageField(upload_to='covers')
+    slug = models.SlugField(unique=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+
